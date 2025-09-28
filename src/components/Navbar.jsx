da1203bb364632185f2b24c20324ca65fe880c97
akashsingh <akashsingh7991421180@gmail.com>
2025-09-28 12:49:04 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-sm-12 bg-primary text-white p-0">
+                <h1 className="text-center">RoomMate</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
